54577fda0607cf7278fbeda1afd083e82f38f6e2
Rukin Danil <rukin2018d@yandex.ru>
2021-05-06 13:05:24 +0700

    Интерполяция многочленами

---
+++ Interpolation/Polynomials/Base/InterpolationPolynomial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompMathLibrary.Interpolation.Polynomials.Base
+{
+	public abstract class InterpolationPolynomial
+	{
+		protected double[] _arguments;
+		protected double[] _values;
+		public abstract double GetFunctionValueIn(double point);
+		public abstract double[] GetFunctionValuesInPoints(double[] points);
+
+		protected int GetMinSizeOfArrays(double[] first, double[] second)
+		{
+			int length;
+			if (first.Length < second.Length)
+				length = first.Length;
+			else
+				length = second.Length;
+			return length;
+		}
+	}
+}
---
+++ Interpolation/Polynomials/NewtonInterpolationPolynomial.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CompMathLibrary.Interpolation.Polynomials.Base;
+
+namespace CompMathLibrary.Interpolation.Polynomials
+{
+	public class NewtonInterpolationPolynomial : InterpolationPolynomial
+	{
+		public override double GetFunctionValueIn(double point)
+		{
+			double result = 0;
+			double num;
+			int n = _values.Length;
+			for (int j = 0; j < n; j++)
+			{
+				num = 1;
+				for (int k = 0; k < j; k++)
+				{
+					num *= (point - _arguments[k]);
+				}
+				result += (num * GetDividedDifferenceOfTheSpecifiedOrder(j + 1, _arguments, _values));
+			}
+			return result;
+		}
+
+		private double GetDividedDifferenceOfTheSpecifiedOrder(int order, double[] args, double[] values)
+		{
+			double result = 0, denominator;
+			for (int i = 0; i < order; i++)
+			{
+				denominator = 1;
+				for (int j = 0; j < order; j++)
+				{
+					if (i != j)
+					{
+						denominator *= (args[i] - args[j]);
+					}
+				}
+				result += values[i] / denominator;
+			}
+			return result;
+		}
+
+		public override double[] GetFunctionValuesInPoints(double[] points)
+		{
+			int size = GetMinSizeOfArrays(points, _values);
+			double[] result = new double[size];
+			double sum;
+			for (int i = 0; i < size; i++) 
+			{
+				sum = 0;
+				for (int j = 0; j < size; j++)
+				{
+					double num = 1;
+					for (int k = 0; k < j; k++)
+					{
+						num *= (points[i] - _arguments[k]);
+					}
+					sum += (num * GetDividedDifferenceOfTheSpecifiedOrder(j + 1, _arguments, _values));
+				}
+				result[i] = sum;
+			}
+			return result;
+		}
+	}
+}
